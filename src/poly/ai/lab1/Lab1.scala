b8cc625104a5b6346788fc9c7f15fd6d73058349
nsokil <nsokil@softserveinc.com>
2017-02-07 17:44:08 +0200

    cli

--- src/poly/ai/lab1/Lab1.scala
+++ src/poly/ai/lab1/Lab1.scala
@@ -1,9 +1,47 @@
 package poly.ai.lab1
 
+import scala.collection.mutable
+import scala.io.StdIn
+
+class REPL {
+  private var u: U[String] = U(Nil)
+  private var bindings: mutable.Map[String, O[String]] = mutable.Map()
+
+  private val REGEX = "([a-zA-Z0-9]+)\\s?\\(\\+|\\-|\\*|\\\\|union){1}\\s?([a-zA-Z0-9]+)"
+
+  def init() = {
+    u = U(invite("U = ").split(" ").toList)
+    bindings = mutable.Map()
+    progress()
+  }
+
+  def progress(): Unit = {
+    val input = invite()
+    val trimmed = input.trim
+    if(trimmed == "U") result(u.o().toString())
+    else if(bindings.contains(trimmed)) result(bindings(trimmed).toString)
+    else if(input.contains("=")) bind(input)
+    else if(input == "reset") init()
+    if (input == "quit" || input == "exit" || input == "q") exit()
+    else progress()
+  }
+
+  private def bind(input: String) = {
+    val parts = input.split("=")
+    bindings(parts(0).trim) = u << parts(1).split(" ").map(_.trim)
+  }
+  private def result(str: String) = println(str)
+  private def invite(): String = invite("")
+  private def invite(msg: String): String = {
+    print(s">> $msg"); StdIn.readLine()
+  }
+
+  private def exit() = result("Exiting.")
+}
+
 object Lab1 {
   def main(args: Array[String]): Unit = {
-    val uni = U(List("a", "b", "c", "d"))
-    val A = uni.produce(List("f", "c", "b"))
-    println(A)
+    val repl = new REPL()
+    repl.init()
   }
 }
